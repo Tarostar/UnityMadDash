2ac504b2faace31e865ade9bb6cc096573b0b360
Claus Bornich <clausb@gmail.com>
2014-07-12 08:20:21 +0200

    Implementation of working game

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	public Transform[] spawnPoints;
+	public GameObject powerup;
+
+	private float timeLeft = 25f;
+	private float timeAddedByPowerup = 2f;
+	private float timeIncrement = 0.05f;
+	private float spawnDelay = 1f;
+	private int powerupsCollected = 0;
+
+	// Use this for initialization
+	void Start () {
+		Screen.showCursor = false;
+		SpawnPowerup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		if (timeLeft <= 0.0f)
+		{
+			print ("Game Over");
+		}
+	}
+
+	void SpawnPowerup()
+	{
+		int location = Random.Range(0, spawnPoints.Length);
+		float xOffset = Random.Range (-1.5f, 1.5f);
+		float zOffset = Random.Range (-1.5f, 1.5f);
+
+		Vector3 pos = spawnPoints[location].position;
+
+		Instantiate (powerup, new Vector3(pos.x + xOffset, pos.y, pos.z + zOffset), Quaternion.identity);
+
+		// call myself again after a delay
+		Invoke("SpawnPowerup", spawnDelay);
+	}
+
+	public void PowerupsCollected()
+	{
+		timeLeft += timeAddedByPowerup;
+		powerupsCollected++;
+
+		spawnDelay += timeIncrement;
+		timeAddedByPowerup += (timeIncrement / 2);
+
+		print (powerupsCollected);
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect(Screen.width / 2 - 10, Screen.height - 70, 90, 40), "Time Left: " + (int)timeLeft);
+		GUI.Label (new Rect(Screen.width / 2 - 10, Screen.height - 50, 90, 40), "Powerups Collected: " + powerupsCollected);
+	}
+}
---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+	private GameControl control;
+
+	private float speed = 0.5f;
+	private float time = 0f;
+	private float distance = 0.01f;
+	private float rotateSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+		control = GameObject.Find ("GameControl").GetComponent<GameControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime * speed;
+		transform.Translate(0f, Mathf.Sin (Mathf.PI * 2 * time) * distance, 0f);
+
+		transform.Rotate (0f, rotateSpeed, 0f);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		control.PowerupsCollected();
+		Destroy (this.gameObject);
+	}
+}
